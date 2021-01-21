acf9b6f5b8d88504d3cdf4f45ae91973d6caeb77
RalucaIonita <raluca.ionita04@gmail.com>
2021-01-21 07:03:51 +0200

    More stuff added

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductsComponent } from './products/products.component';
 import { CartComponent } from './cart/cart.component';
+import { NavBarComponent } from './products/nav-bar/nav-bar/nav-bar.component';
 
 
 
 @NgModule({
-  declarations: [ProductsComponent, CartComponent],
+  declarations: [ProductsComponent, CartComponent, NavBarComponent],
   imports: [
     CommonModule
   ]
