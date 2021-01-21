0c99e168b0805087e8b36f5ac8b2d7458a181682
RalucaIonita <raluca.ionita04@gmail.com>
2021-01-21 05:47:23 +0200

    Feeling proud here

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './account/login/login.component';
+import { RegisterComponent } from './account/register/register.component';
+import { ProductsComponent } from './store/products/products.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'products', component: ProductsComponent}
 ];
 
 @NgModule({
